cd13f25ca85d9ede6f520d8f1c7d0c3ca1fbeed6
Allen Guo <guosonglin001@gmail.com>
2024-03-04 23:05:46 +0800

    clean scripts (#5)

--- test/t.sh
+++ test/t.sh
@@ -1,5 +1,9 @@
+# https://docs.docker.com/build/building/multi-platform/
+docker run --privileged --rm tonistiigi/binfmt --install all
+
 docker buildx create --name multiarch --driver docker-container --use
 
+# pwd is `test/`
 # docker buildx build \
 #     --platform=linux/amd64,linux/arm64 \
 #     --load \
@@ -8,36 +12,32 @@ docker buildx create --name multiarch --driver docker-container --use
 docker buildx build \
     --platform=linux/arm64 \
     --load \
-    -t test:arm64 .
+    -t test:arm64 \
+    -f test/Dockerfile .
 
 docker buildx build \
     --platform=linux/amd64 \
     --load \
-    -t test:amd64 .
+    -t test:amd64 \
+    -f test/Dockerfile .
 
 docker buildx build \
     --platform=linux/riscv64 \
     --load \
     -t test:riscv64 \
-    -f Dockerfile.riscv64 .
+    -f test/Dockerfile.riscv64 .
 
 docker buildx build \
     --platform=linux/riscv64 \
     --load \
     -t test:riscv64.sid \
-    -f Dockerfile.riscv64.sid .
+    -f test/Dockerfile.riscv64.sid .
 
 # clean multiarch
 docker buildx rm multiarch
 # check current driver
 docker buildx ls
 
-# temp
-# --platform=linux/amd64,linux/arm64,linux/riscv64 \
-# --platform=linux/amd64,linux/arm64 \
-# --platform=linux/amd64 \
-# --platform=linux/amd64,linux/riscv64 \
-# --platform=linux/amd64,linux/arm64 \
-# --platform=linux/amd64,linux/386 \
-
-# docker run --privileged --rm tonistiigi/binfmt --install all
+# run test:riscv64.sid
+docker run --rm -it --platform linux/riscv64 test:riscv64.sid bash
+docker run --rm -it test:riscv64.sid bash

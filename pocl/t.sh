d0a1d4e52d40eb496a6a020f3ca8136d435cfdb2
Allen Guo <guosonglin001@gmail.com>
2024-03-24 16:57:01 +0800

    wip

--- pocl/t.sh
+++ pocl/t.sh
@@ -3,6 +3,8 @@
 # https://docs.docker.com/build/building/multi-platform/
 docker run --privileged --rm tonistiigi/binfmt --install all
 docker buildx create --name multiarch --driver docker-container --use
+# after building
+# docker buildx rm multiarch
 
 docker buildx build --platform=linux/riscv64 --load -t pocl:latest-riscv -f pocl/Dockerfile.riscv .
 
